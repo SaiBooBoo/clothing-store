ba3c59434d4b26c727bd465cb1a56025fd0498b0
Sai Tai Mao <109742655+SaiBooBoo@users.noreply.github.com>
2025-08-18 15:41:08 +0630

    Adding Backend Api

---
+++ backend/store-api/src/main/java/com/shen/store/api/StoreApiWebConfig.java
@@ -0,0 +1,57 @@
+package com.shen.store.api;
+
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+
+@Configuration
+@PropertySource(value = "classpath:/commons.properties")
+public class StoreApiWebConfig implements WebMvcConfigurer {
+
+	@Value("@{spring.mvc.format.data}")
+	private String dateFormat;
+	@Value("${spring.mvc.format.date-time}")
+	private String dateTimeFormat;
+	@Value("${app.subscription.slip-directory}")
+	private String slipDirectory;
+
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+		.allowedHeaders("*")
+		.allowedOrigins("*")
+		.allowedMethods("*");
+	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		var slips = Paths.get(slipDirectory).toAbsolutePath().normalize();
+		var slipsLocation = "file:%s/".formatted(slips.toString());
+		registry.addResourceHandler("/slips/**").addResourceLocations(slipsLocation);
+	}
+	
+	@Bean
+	Jackson2ObjectMapperBuilderCustomizer objectMapperBuilderCustomizer() {
+		return builder -> {
+			builder.serializerByType(LocalDate.class, new LocalDateSerializer(DateTimeFormatter.ofPattern(dateFormat)));
+			builder.serializerByType(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(dateTimeFormat)));
+			builder.deserializerByType(LocalDate.class, new LocalDateDeserializer(DateTimeFormatter.ofPattern(dateFormat)));
+			builder.deserializerByType(LocalDateTime.class, new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern(dateTimeFormat)));
+		};
+	}
+}
